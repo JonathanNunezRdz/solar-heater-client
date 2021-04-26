846afc1f4c74bf55ed2ed3fac86186541d8af467
JonathanNunezRdz <jonathannunezr1@gmail.com>
2021-04-25 23:29:48 -0500

    api call successfull

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import api from './services/api';
+// import { Canvas } from '@react-three/fiber';
+// import Box from './Box';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// return (
+	// 	<Canvas>
+	// 		<ambientLight />
+	// 		<pointLight position={[10, 10, 10]} />
+	// 		<Box position={[-2, 0, 0]} />
+	// 		<Box position={[2, 0, 0]} />
+	// 	</Canvas>
+	// );
+	const [rotation, setRotation] = useState({ x: 0.0, y: 0.0 });
+	const [active, setActive] = useState(false);
+
+	const requestActive = async () => {
+		const { data } = await api.get();
+		if (data.message === 'mpu6050 turned on') setActive(true);
+	};
+
+	const requestRotation = async () => {
+		const { data } = await api.get('/mpu');
+		setRotation({ x: data.x_rotation, y: data.y_rotation });
+	};
+
+	return (
+		<Container>
+			<Row>
+				<Col xs={6} className="text-center">
+					<Button onClick={requestRotation} disabled={!active}>
+						Request Rotation
+					</Button>
+					<h1>X: {rotation.x.toFixed(2)}</h1>
+					<h1>Y: {rotation.y.toFixed(2)}</h1>
+				</Col>
+				<Col xs={6} className="text-center">
+					<Button onClick={requestActive}>Request Active</Button>
+					<h1>Sensor Status: {active ? 'On' : 'Off'}</h1>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
 export default App;
